5c170daee3d3bccd5e01d6cf5e20f4d91a30e823
Adonias Pessoa <82778067+Adonias1994@users.noreply.github.com>
2026-09-21 00:07:18 -0300

    Delete Problema 2 - Extremamente Básico directory

--- Problema 2 - Extremamente Básico/Extremamente Básico.py
+++
@@ -1,7 +0,0 @@
-#1º Ler 2 valores inteiros e armazene-os nas variáveis A e B.
-A = int(input())
-B = int(input())
-# 2º Efetar a soma de A e B atribuindo o seu resultado na variável X.
-# 3º Imprimir X. Não apresente mensagem alguma além daquilo que está sendo especificado.
-print('X =', A+B)
-#4º Não esqueça de imprimir o fim de linha após o resultado.
